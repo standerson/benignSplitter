39cdbfa00dde18ee40c2c217bd6e0baa2bc5334a
standerson <steven.anderson4@gmail.com>
2017-11-28 16:37:58 -0600

    remove recipient storage

--- Splitter.sol
+++ Splitter.sol
@@ -2,23 +2,26 @@ pragma solidity ^0.4.8;
 
 contract Splitter {
 
-	address[] public recipients;
 	address owner;
-
+	
 	function Splitter() {
 		owner = msg.sender;  
 	}
 
 	function splitFunds(address[] _recipients) payable {
-		
-		recipients = _recipients;
 
 		uint numRecipeints = recipients.length;
-		uint splitAmount = msg.value / numRecipeints;
-
-		for (uint i=0; i<numRecipeints; i++){
+		uint splitAmount = msg.value / numRecipients;
+		
+		if(msg.gas<(numRecipients*21000)){
+			throw;
+		}
+		
+		for (uint i=0; i<numRecipients; i++){
 			recipients[i].transfer(splitAmount); 
 		}
+		
+		
 	}
 
 	function destroy() {
@@ -26,4 +29,6 @@ contract Splitter {
 			selfdestruct(owner);
 		}
 	}
+	
+	
 }
