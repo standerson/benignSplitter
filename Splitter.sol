8a7ca35cbd8617a34584a525c80b2d0cf6ac187b
standerson <steven.anderson4@gmail.com>
2017-11-28 20:09:58 -0600

    Guard and Suicide gracefully
    Destroy no longer suicides add resurrect. Guard against out of gas and recipients less than 2.

--- Splitter.sol
+++ Splitter.sol
@@ -3,21 +3,32 @@ pragma solidity ^0.4.8;
 contract Splitter {
 
 	address owner;
+	bool isActive = true;
 	
+	modifier onlyActive () {
+		require (isActive == true);
+		_;
+	}
 	function Splitter() {
+
 		owner = msg.sender;  
 	}
 
-	function splitFunds(address[] _recipients) payable {
+	function onlyActive splitFunds(address[] _recipients) payable {
 
-		uint numRecipeints = recipients.length;
+		uint numRecipients = recipients.length;
 		uint splitAmount = msg.value / numRecipients;
+		uint remainder = msg.value - (splitAmount * numRecipients);
 		
-		if(msg.gas<(numRecipients*21000)){
+		
+		if(msg.gas<(numRecipients*21000)||numRecipients<2){
 			throw;
 		}
 		
 		for (uint i=0; i<numRecipients; i++){
+			if (i == numRecipients - 1){
+				splitAmount += remainder; 
+			}
 			recipients[i].transfer(splitAmount); 
 		}
 		
@@ -26,7 +37,13 @@ contract Splitter {
 
 	function destroy() {
 		if (msg.sender == owner) {
-			selfdestruct(owner);
+			isActive = false;
+		}
+	}
+
+	function resurrect() {
+		if (msg.sender == owner) {
+			isActive = true;
 		}
 	}
 	
